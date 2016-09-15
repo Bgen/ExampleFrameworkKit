02ce8c83807135c456f2e1c6af8ad81ac998670f
Burcu Geneci <burcugeneci@caretta.local>
2016-09-15 13:05:06 +0300

    Initial Commit

---
+++ ExampleFrameworkKit/ExampleFrameworkKit.h
@@ -0,0 +1,19 @@
+//
+//  ExampleFrameworkKit.h
+//  ExampleFrameworkKit
+//
+//  Created by Burcu Geneci on 15/09/16.
+//  Copyright © 2016 Burcu Geneci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleFrameworkKit.
+FOUNDATION_EXPORT double ExampleFrameworkKitVersionNumber;
+
+//! Project version string for ExampleFrameworkKit.
+FOUNDATION_EXPORT const unsigned char ExampleFrameworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleFrameworkKit/PublicHeader.h>
+
+
